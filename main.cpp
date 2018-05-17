5542b817d93c3dfa95b9109fd58074e06e7631b0
Tanya <tbatsenko@gmail.com>
2018-05-17 15:18:21 +0300

    local changes

--- main.cpp
+++ main.cpp
@@ -39,62 +39,8 @@ void accept_and_run(ip::tcp::acceptor& acceptor, io_service& io_service)
     });
 }
 
-//std::thread run_thread(
-//        std::bind(static_cast<size_t(boost::asio::io_service::*)()>(
-//                          &boost::asio::io_service::run), std::ref(io_service)));
-
 int main(int argc, const char * argv[])
 {
-    io_service ioservice;
-
-    int NUM_THREADS = 2;
-
-    boost::asio::io_service::work some_work(ioservice);
-
-
-    const ip::tcp::endpoint endpoint{ip::tcp::v4(), 3000};
-    ip::tcp::acceptor acceptor{ioservice, endpoint};
-
-//    acceptor_.bind({ ip::tcp::v4(), 6767 });
-
-//    acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-//    acceptor.bind(endpoint);
-
-    acceptor.listen();
-
-
-
-
-    boost::thread_group threads;
-    for (int i = 0; i < NUM_THREADS; ++i) {
-        threads.create_thread(boost::bind(&io_service::run, &some_work.get_io_service()));
-    }
-
-    accept_and_run(acceptor, ioservice);
-
-    ioservice.run();
-
-    threads.join_all();
-
-
-
-
-
-//
-//    const int THREADS_AMOUNT = 2;
-//    std::vector<std::thread> threads;
-//
-//    for (int i = 0; i < THREADS_AMOUNT; ++i) {
-//
-//        threads.emplace_back([&ioservice](){ ioservice.run(); });
-//    }
-//
-//    for (int i = 0; i < THREADS_AMOUNT; ++i) {
-//        threads[i].join();
-//    }
-
-
-//    ioservice.run();
 
     return 0;
 }
